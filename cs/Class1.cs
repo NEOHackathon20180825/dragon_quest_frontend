c229190efa82fe3c8652443d698b32fac424df95
ookimaki <ookimaki@givinglog.com>
2018-08-25 20:01:46 +0900

    modify cs

--- cs/Class1.cs
+++ cs/Class1.cs
@@ -13,20 +13,46 @@ namespace Neo.SmartContract
         public static Object Main(string operation, params object[] args)
         {
             if (operation == "name") return Name();
+            if (operation == "setString") return SetString((string)args[0]);
+            if (operation == "setNum") return SetNum((ulong)args[0]);
+            if (operation == "getString") return GetString();
+            if (operation == "getNum") return GetNum();
             if (operation == "bool") return true;
             if (operation == "totalSupply") return TotalSupply();
             return NotifyErrorAndReturnFalse("operation not found");
+        }
+
+        public static bool SetString(string value)
+        {
+            Storage.Put(Storage.CurrentContext, "valueString", value);
+            return true;
+        }
+
+        public static bool SetNum(ulong value)
+        {
+            Storage.Put(Storage.CurrentContext, "valueNum", value);
+            return true;
+        }
+
+        public static string GetString()
+        {
+            return Storage.Get(Storage.CurrentContext, "valueString").AsString();
+        }
+
+        public static BigInteger GetNum()
+        {
+            return Storage.Get(Storage.CurrentContext, "valueNum").AsBigInteger();
+        }
+
+        public static BigInteger TotalSupply()
+        {
+            return Storage.Get(Storage.CurrentContext, "totalSupply").AsBigInteger();
         }
 
         public static bool Deploy()
         {
             Storage.Put(Storage.CurrentContext, "totalSupply", total_amount);
             return true;
-        }
-
-        public static BigInteger TotalSupply()
-        {
-            return Storage.Get(Storage.CurrentContext, "totalSupply").AsBigInteger();
         }
 
         public static bool NotifyErrorAndReturnFalse(string value)
