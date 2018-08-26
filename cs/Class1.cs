3be8dc71cf3303cae79678bcc3fc141636b5d8c1
ookimaki <ookimaki@givinglog.com>
2018-08-26 13:58:30 +0900

    add class

--- cs/Class1.cs
+++ cs/Class1.cs
@@ -7,10 +7,23 @@ namespace Neo.SmartContract
 {
     public class Class1 : Framework.SmartContract
     {
-    
-        public static String Main()
+        public const ulong value = 0;
+
+        public static BigInteger Main(string operation)
         {
-            return "Dragon Questions!!";
+            if (operation == "set") {
+                BigInteger like = 1;
+                BigInteger newValue = Storage.Get(Storage.CurrentContext, "like").AsBigInteger() + like;
+                Storage.Put(Storage.CurrentContext, "like", newValue);
+                return value;
+            }
+
+            if (operation == "get")
+            {
+                return Storage.Get(Storage.CurrentContext, "like").AsBigInteger();
+            }
+
+            return value;
         }
 
     }
