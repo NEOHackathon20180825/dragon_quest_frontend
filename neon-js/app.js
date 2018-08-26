99efec85862f45df89deb85df49b52030a6c4083
ookimaki <ookimaki@givinglog.com>
2018-08-26 12:21:04 +0900

    add

--- neon-js/app.js
+++ neon-js/app.js
@@ -1,70 +1,39 @@
-const contract = 'e0cad06f15a4684595574c5cb95a2ed3dd2c0c63';
+function getTitle() {
 
-function get(method) {
-/*
+    const contract = '60dea68f62ef03df87ae7c1079a64a9d1aff031f';
     const props = {
-        scriptHash: contract, 
-        operation: method,
-        args: [],
+        scriptHash: contract,
     }
-
     const script = Neon.default.create.script(props)
 
-    const config = {
-        net: "http://localhost:4000/api/main_net/",
-        name: 'PrivateNet',
-        extra: {
-            neoscan: 'http://localhost:4000/api/main_net'
-        },
-        script: script,
-        address: 'AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y',
-        privateKey: 'KxDgvEKzgSBPPfuVfw67oPQBSjidEiqTHURKSDL1R7yGaGYAeYnr',
-        filters: "",
-        gas: 1
-    }
-
-    const privateNet = new Neon.rpc.Network(config)
-    Neon.default.add.network(privateNet)
-
-    Neon.default.doInvoke(config).then(res => {
-        console.log(res)
-      })
-*/
-
-const props = {
-    scriptHash: contract, 
-    //operation: method,
-    //args: [],
-}
-const script = Neon.default.create.script(props)
-
-Neon.rpc.Query.invokeScript(script)
-    .execute('http://localhost:30333')
-    .then(res => {
-        console.log(res)    
-    })
+    Neon.rpc.Query.invokeScript(script)
+        .execute('http://localhost:30333')
+        .then(res => {
+            console.log(Neon.u.hexstring2str(res.result.stack[1].value));
+            return Neon.u.hexstring2str(res.result.stack[1].value);
+        })
 }
 
-function sendNeo(){
+function sendNeo() {
     Neon.api.default.sendAsset({
         net: 'http://localhost:4000/api/main_net/',
         address: "AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y",
         privateKey: "KxDgvEKzgSBPPfuVfw67oPQBSjidEiqTHURKSDL1R7yGaGYAeYnr",
         intents: Neon.api.makeIntent({
-          GAS: 1,
-          NEO: 1,
+            GAS: 1,
+            NEO: 1,
         }, "AR3uEnLUdfm1tPMJmiJQurAXGL7h3EXQ2F"),
-      })
-      .then(rpcResponse => {
-        console.log(rpcResponse)
-      });
+    })
+        .then(rpcResponse => {
+            console.log(rpcResponse)
+        });
 }
 
 
 
 function set(method) {
     const props = {
-        scriptHash: contract, 
+        scriptHash: contract,
         operation: method,
         args: [],
     }
@@ -89,30 +58,7 @@ function set(method) {
 
     Neon.default.doInvoke(config).then(res => {
         console.log(res)
-      })
+    })
 
 }
 
-
-
-/*
-function setString(method, param) {
-    const props = {
-        scriptHash: contract, 
-        operation: method,
-        args: [Neon.u.str2hexstring(param)],
-    }
-    const script = Neon.default.create.script(props)
-
-    Neon.rpc.Query.invokeScript(script)
-        .execute('http://seed3.neo.org:20332')
-        .then(res => {
-            console.log(res)    
-        })
-}
-
-function getBalance() {
-    Neon.api.neoscan.getBalance('PrivateNet', "AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y")
-        .then(res => console.log(res))
-}
-*/
